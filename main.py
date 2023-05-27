591092b3601a96f6f32e8a6ed78a55d1922b439e
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-05-27 17:09:42 +0200

    More work done on digit classification

--- main.py
+++ main.py
@@ -1,41 +1,11 @@
 import pygame
-import tensorflow as tf
 from tensorflow import keras
-from keras.datasets import mnist
-from keras import layers
-import matplotlib.pyplot as plt
+import cv2
+import numpy as np
 
 
 WIN = pygame.display.set_mode((28, 28))
 
-
-def create_model():
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
-    X_train = X_train.reshape(X_train.shape + (1,))
-    X_test = X_test.reshape(X_test.shape + (1,))
-    X_train = X_train / 255.
-    X_test = X_test / 255.
-    X_train = X_train.astype('float32')
-    X_test = X_test.astype('float32')
-    model = tf.keras.Sequential([
-        layers.Conv2D(filters=10,
-                    kernel_size=3, 
-                    activation="relu", 
-                    input_shape=(28,  28,  1)),
-        layers.Conv2D(10,  3, activation="relu"),
-        layers.MaxPool2D(),
-        layers.Conv2D(10,  3, activation="relu"),
-        layers.Conv2D(10,  3, activation="relu"),
-        layers.MaxPool2D(),
-        layers.Flatten(),
-        layers.Dense(10, activation="softmax")
-    ])
-    model.compile(loss="sparse_categorical_crossentropy", 
-                optimizer=tf.keras.optimizers.Adam(),
-                metrics=["accuracy"])
-    model.fit(X_train, y_train, epochs=10)
-    return model 
-    
 def translate_colors(colors):
     for i in range(len(colors)):
         for j in range(len(colors[i])):
@@ -46,7 +16,7 @@ def translate_colors(colors):
     return colors
 
 def main():
-    model = create_model()
+    model = keras.models.load_model("digit-recognizer.h5")
     WIN.fill((255, 255, 255))
     pygame.display.update()
     while True:
@@ -66,7 +36,12 @@ def main():
 
         if pygame.key.get_pressed()[pygame.K_a]:
             matrix = pygame.surfarray.array2d(WIN)
-            correct_matrix = translate_colors(matrix).transpose()
+            correct_matrix = translate_colors(matrix)
+            # image = np.expand_dims(correct_matrix, axis=-1).astype(np.float32)
+            # correct_matrix = cv2.resize(image, (28, 28))
+            # resized_matrix = np.squeeze(correct_matrix)
+
+            # ans = model.predict(resized_matrix.reshape(1, 28, 28, 1))
             ans = model.predict(correct_matrix.reshape(1, 28, 28, 1))
             print(ans.argmax())
         
