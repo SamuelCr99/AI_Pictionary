13af0c22d51bdaae940529d56f3e572118a82cf3
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-07-12 23:50:33 -0400

    Added main loop

--- ai_pictionary.py
+++ ai_pictionary.py
@@ -5,12 +5,15 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 import random
+import time
 
 
-WIN = pygame.display.set_mode((800,800))
+WIN = pygame.display.set_mode((800,900))
 
 
 def main():
+    pygame.init()
+    font = pygame.font.SysFont('Arial', 30)
     model = keras.models.load_model("drawing_recognizer.h5")
     model.predict(np.zeros((1, 28, 28, 1))) # For some reason the first predict is slow. 
                                             # So this is run so all future predicts are quick
@@ -21,7 +24,6 @@ def main():
     WIN.fill((255, 255, 255))
     pygame.display.update()
     item_to_draw = list(data.values())[random.randint(0,len(data)-1)]
-    print(f'Item to draw: {item_to_draw}')
 
     while True:
         for event in pygame.event.get():
@@ -31,7 +33,6 @@ def main():
 
         while pygame.mouse.get_pressed()[0]:
             # Draw a rect around the mouse
-            r = pygame.Rect(pygame.mouse.get_pos(), (10, 10))
             pygame.draw.circle(WIN, (0, 0, 0), pygame.mouse.get_pos(), 13)
             pygame.display.update()
             pygame.event.pump()
@@ -43,10 +44,14 @@ def main():
 
         if pygame.key.get_pressed()[pygame.K_SPACE]:
             WIN.fill((255, 255, 255))
-            pygame.display.update()
+
+        if pygame.key.get_pressed()[pygame.K_r]:
+            item_to_draw = list(data.values())[random.randint(0,len(data)-1)]
+            WIN.fill((255, 255, 255))
 
         if pygame.key.get_pressed()[pygame.K_a]:
             matrix = pygame.surfarray.array2d(WIN)
+            matrix = matrix[:, 100:]
             matrix = np.transpose(matrix)
             image = np.expand_dims(matrix, axis=-1).astype(np.float32)
             image = abs((image - 16777215.) / 16777215.)
@@ -57,10 +62,16 @@ def main():
             guess = data[str(ans.argmax())]
             print(f'My guess is: {guess}')
             if guess == item_to_draw: 
-                print('Correct, you win!')
-                quit()
+                print('Correct!')
+                WIN.fill((255, 255, 255))
+                item_to_draw = list(data.values())[random.randint(0,len(data)-1)]
+
             else:
                 print('Hmm incorrect, try again!')
+        text = font.render(f'Item to draw: {item_to_draw}', True, (0, 0, 0))
+        textRect = text.get_rect()
+        WIN.blit(text, textRect)
+        pygame.display.update()
         
 
 
