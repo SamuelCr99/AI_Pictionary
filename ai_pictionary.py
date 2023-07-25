ae57f83d7f5a3a832c0eff696ad350c39b7a239a
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2023-07-24 23:31:18 -0400

    Added comments

--- ai_pictionary.py
+++ ai_pictionary.py
@@ -14,25 +14,38 @@ from pygame_widgets.button import Button
 WIN = pygame.display.set_mode((800, 930))
 
 
-# Colors
+# Colors 
 BLACK = ((0, 0, 0))
 WHITE = ((255, 255, 255))
 GREEN = ((0, 255, 0))
+BLUE = ((50, 50, 255))
+RED = ((255, 0 , 0))
 
 item_to_draw = ""
 
 
 def predict(data, model, r):
+    """
+    Predicts what the drawing is
+
+    Parameters: 
+    data [dict]: Dictionary containing mapping between numbers and labels 
+    model: Tensorflow model for doing predictions 
+    r: Drawing window 
+    """
     global item_to_draw
 
+    # Get matrix of drawing area
     matrix = pygame.surfarray.array2d(WIN)
     matrix = matrix[:, 50:850]
     matrix = np.transpose(matrix)
+
+    # Convert matrix to correspond to the training data
     image = np.expand_dims(matrix, axis=-1).astype(np.float32)
     image = abs((image - 16777215.) / 16777215.)
-
     image = cv2.resize(image, (28, 28))
 
+    # Predict the drawing against the model and get 3 guesses
     ans_arr = model.predict(image.reshape(1, 28, 28, 1))
     ans_copy = copy.deepcopy(ans_arr)
     guess1 = data[str(ans_copy.argmax())]
@@ -45,9 +58,10 @@ def predict(data, model, r):
     print(f'My second guess is: {guess2}')
     print(f'My third guess is: {guess3}')
 
+    # If guess is correct clear the screen and create a new item to draw
     if item_to_draw in [guess1, guess2, guess3]:
         print('Correct!')
-        WIN.fill(WIN, r)
+        WIN.fill(WHITE, r)
         item_to_draw = list(data.values())[random.randint(0, len(data)-1)]
 
     else:
@@ -55,8 +69,18 @@ def predict(data, model, r):
 
 
 def new_object(data, win, draw_window_rect, text_rect):
+    """
+    Clears screen and updates object to draw
+
+    Parameters: 
+    data [dict]: Dictionary containing mapping between numbers and labels 
+    win: Main widow
+    draw_window_rect: Drawing window
+    text_rect: Rect for showing item to predict text
+    """
     global item_to_draw
 
+    # Choose a new random value and clear the screen
     item_to_draw = list(data.values())[random.randint(0, len(data)-1)]
     win.fill(WHITE, draw_window_rect)
     win.fill(WHITE, text_rect)
@@ -64,21 +88,28 @@ def new_object(data, win, draw_window_rect, text_rect):
 
 def main():
     global item_to_draw
+
+    # Initialize pygame and font and load tensorflow model
     pygame.init()
-    font = pygame.font.SysFont('Arial', 30)
+    font = pygame.font.SysFont('Monospace', 30)
     model = keras.models.load_model("drawing_recognizer.h5")
-    # For some reason the first predict is slow.
+
+    # For some reason the first predict is slow. Therefor we do it when the 
+    # program starts
     model.predict(np.zeros((1, 28, 28, 1)))
-    # So this is run so all future predicts are quick
-    # Read the json file
+
+    # Read the json file containing mapping between numbers and labels
     with open('num_to_image_dict.json', 'r') as f:
         data = json.load(f)
 
+    # Rects for the drawing area and text area
     draw_window_rect = pygame.Rect(0, 50, 800, 800)
     text_rect = pygame.Rect(0, 0, 800, 30)
 
     WIN.fill((255, 255, 255))
     pygame.display.update()
+
+    # Choose a random item to draw
     item_to_draw = list(data.values())[random.randint(0, len(data)-1)]
 
     while True:
@@ -89,7 +120,8 @@ def main():
                 exit()
 
         while pygame.mouse.get_pressed()[0]:
-            # Draw a rect around the mouse
+            # Draw a circle around the mouse when mouse 1 is pressed, allow 
+            # circles to be drawn inside the draw rect 
             mouse_pos = pygame.mouse.get_pos()
             if mouse_pos[1] < 50 or mouse_pos[1] > 850:
                 break
@@ -98,25 +130,33 @@ def main():
             pygame.event.pump()
 
         while pygame.mouse.get_pressed()[2]:
+            # Clear a circle around the mouse when mouse 2 is pressed
+            mouse_pos = pygame.mouse.get_pos()
+            if mouse_pos[1] < 50 or mouse_pos[1] > 850:
+                break
             pygame.draw.circle(WIN, (255, 255, 255),
-                               pygame.mouse.get_pos(), 17)
+                               mouse_pos, 17)
             pygame.display.update()
             pygame.event.pump()
 
+        # Draw text to screen
         text = font.render(f'Item to draw: {item_to_draw}', True, (0, 0, 0))
         textRect = text.get_rect()
         WIN.blit(text, textRect)
+
+        # Draw lines 
         upper_line = pygame.Rect(0, 30, 800, 20)
         lower_line = pygame.Rect(0, 850, 800, 20)
         pygame.draw.rect(WIN, BLACK, upper_line)
         pygame.draw.rect(WIN, BLACK, lower_line)
 
+        # Add buttons for predicting, clearing and new item
         Button(WIN, 690, 875, 100, 50, text="Predict", inactiveColour=GREEN,
                onClick=lambda: predict(data, model, draw_window_rect))
         Button(WIN, 580, 875, 100, 50, text="Clear Image",
-               onClick=lambda: WIN.fill(WHITE, draw_window_rect))
+               onClick=lambda: WIN.fill(WHITE, draw_window_rect), inactiveColour=BLUE)
         Button(WIN, 470, 875, 100, 50, text="New Object", onClick=lambda: new_object(
-            data, WIN, draw_window_rect, text_rect))
+            data, WIN, draw_window_rect, text_rect), inactiveColour=RED)
 
         pygame_widgets.update(events)
         pygame.display.update()
--- create_drawing_model.py
+++ create_drawing_model.py
@@ -1,68 +1,88 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import random
 import tensorflow as tf
 import json
 from keras import layers
 from sklearn.model_selection import train_test_split
 
 
-x = []
-y = []
+def create_model(train_data_location):
+    """
+    Creates a convolutional neural network for predicting doodles
 
-dict = {}
+    Parameters: 
+    train_data_location[str]: The file path of the training data. This training 
+    data should be numpy bitmaps. Bitmaps can be downloaded here: https://console.cloud.google.com/storage/browser/quickdraw_dataset/full/numpy_bitmap
+    
+    
+    """
 
-c = -1
-for bit_map in os.listdir('data'):
-    data = np.load('data/' + bit_map, encoding='latin1', allow_pickle=True)
-    c += 1
-    dict[c] = bit_map[18:-4]
-    for i in range(10000):
-        x.append(data[i].reshape((28, 28)))
-        y.append(c)
+    # Lists for holding data and labels
+    x = []
+    y = []
 
+    # Dictionary mapping between labels and numbers 
+    dict = {}
 
-# Split x and y into train and test
-X_train, X_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.2, random_state=42)
-X_train = np.array(X_train)
-X_test = np.array(X_test)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
-
-
-X_train = X_train.reshape(X_train.shape + (1,))
-X_test = X_test.reshape(X_test.shape + (1,))
-X_train = X_train / 255.
-X_test = X_test / 255.
-
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-y_train = y_train.astype('uint8')
-y_test = y_test.astype('uint8')
-
-model = tf.keras.models.Sequential()
-model.add(layers.Conv2D(10, (3, 3), activation='relu', input_shape=(28, 28, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(20, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(20, (3, 3), activation='relu'))
-model.add(layers.Flatten())
-model.add(layers.Dense(20, activation='relu'))
-model.add(layers.Dense(c+1, activation='softmax'))
+    c = -1
+    for bit_map in os.listdir(train_data_location):
+        data = np.load(f"{train_data_location}/{bit_map}", encoding='latin1', allow_pickle=True)
+        c += 1
+        dict[c] = bit_map[18:-4]
+        # Trains each label on 10000 images
+        for i in range(10000):
+            x.append(data[i].reshape((28, 28)))
+            y.append(c)
 
 
-model.compile(loss="sparse_categorical_crossentropy",
-              optimizer=tf.keras.optimizers.Adam(),
-              metrics=["accuracy"])
-model.fit(X_train, y_train, epochs=4, validation_data=(X_test, y_test))
+    # Split x and y into train and test
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.2, random_state=42)
 
-test_loss, test_acc = model.evaluate(X_test, y_test)
-print(test_acc)
+    # Cast all lists to np arrays 
+    X_train = np.array(X_train)
+    X_test = np.array(X_test)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
 
-model.save("drawing_recognizer.h5")
 
-# Dump the dict to a json file
-with open('num_to_image_dict.json', 'w') as f:
-    json.dump(dict, f, indent=4)
+    X_train = X_train.reshape(X_train.shape + (1,))
+    X_test = X_test.reshape(X_test.shape + (1,))
+
+    # Normalize the values 
+    X_train = X_train / 255.
+    X_test = X_test / 255.
+
+    X_train = X_train.astype('float32')
+    X_test = X_test.astype('float32')
+    y_train = y_train.astype('uint8')
+    y_test = y_test.astype('uint8')
+
+    model = tf.keras.models.Sequential()
+    model.add(layers.Conv2D(10, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(20, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(20, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(20, activation='relu'))
+    model.add(layers.Dense(c+1, activation='softmax'))
+
+
+    model.compile(loss="sparse_categorical_crossentropy",
+                optimizer=tf.keras.optimizers.Adam(),
+                metrics=["accuracy"])
+    model.fit(X_train, y_train, epochs=4, validation_data=(X_test, y_test))
+
+    test_loss, test_acc = model.evaluate(X_test, y_test)
+    print(test_acc)
+
+    model.save("drawing_recognizer.h5")
+
+    # Dump the dict to a json file
+    with open('num_to_image_dict.json', 'w') as f:
+        json.dump(dict, f, indent=4)
+
+if __name__ == "__main__":
+    create_model("data")
